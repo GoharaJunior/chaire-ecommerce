c1f18b1a9de5d37ca58ed7e993b6e5e50d89ddd3
Gohara Junior <goharajunior@gmail.com>
2020-08-15 16:43:56 -0300

    Ajustes na Arrow

--- js/sliders.js
+++ js/sliders.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $('.fotos_modelos').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    centerMode: true,
+    prevArrow: $("#arrow-prev"),
+    nextArrow: $("#arrow-next")
   });
 });
